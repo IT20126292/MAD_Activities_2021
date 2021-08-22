1ae0d3673b58c8ea811002df1456cad3daf7a72f
Kavisha Silva <it20126292@my.sliit.lk>
2021-08-22 20:16:07 +0530

    New_Updates

--- app/src/main/java/com/example/tempreture_cal/MainActivity.java
+++ app/src/main/java/com/example/tempreture_cal/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
          btn_Cal.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
+
                  calculateAnswer();
              }
          });
